270b77e20d3910f6d7c07735b402e05c704bade3
koplenov <koplenov@gmail.com>
2023-05-09 01:32:01 +0300

    flush project

---
+++ view.ts
@@ -0,0 +1,25 @@
+
+namespace $.$$ {
+
+	export class $koplenov_reactor extends $.$koplenov_reactor {
+		@$mol_mem
+		peoples(){
+			return $mol_fetch.json("https://reactor.kinsle.ru/users") as any
+		}
+
+		@$mol_mem_key
+		content(people: any) {
+			return JSON.stringify($mol_fetch.json(`https://reactor.kinsle.ru/users/${people.id}`), undefined, 2)
+		}
+
+		@$mol_mem
+		peoples_view() {
+			return this.peoples().map((people: any) => this.PeopleView(people))
+		}
+
+		@$mol_mem_key
+		people_name(people: any){
+			return people.username
+		}
+	}
+}
